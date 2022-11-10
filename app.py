9d8bb2eee22f3095dfb8b3fc37a953e337c010ba
Edwinssenkabirwa <edwinssenkabirwa@gmail.com>
2022-11-10 17:23:45 +0300

    sign up form  is validated but still with some errors

--- app.py
+++ app.py
@@ -9,13 +9,32 @@ def welcome_page():
 
 @app.route('/sign_up', methods=['POST','GET'])
 def sign_up():
+    error=None
     if request.method =='POST':
         Firstname=request.form.get('firstname')
         Lastname=request.form.get('lastname')
         Email=request.form.get('email')
         Passwd=request.form.get('password')
 
-    return render_template('sign_up.html')
+        for i in Firstname,Lastname:
+            numbers=('1','2','3','4','5','6','7','8','9','0')
+            signs=('!','@','#','$','%','^','&','*')
+            if i in numbers:
+                error='names should not contain numbers'
+            elif i in signs:
+                error='names should not contain signs'
+            elif len('Firstname')<2:
+                error='Your name must atleast contain 2 characters'
+            elif len('Firstname')>20:
+                error='Your name must atmost contain 20 characters'
+            elif '@' not in Email:
+                error='invalid email: Emails must contain "@" character'
+            elif len(Passwd)<8:
+                error= 'Your password must atleast have 8 characters'
+            else:
+                pass
+
+    return render_template('sign_up.html', error=error)
 
 
 @app.route('/login')

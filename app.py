92e2042647b28ecc7156708d689c7e5b100f5bfb
Edwinssenkabirwa <edwinssenkabirwa@gmail.com>
2022-11-17 16:20:40 +0300

    form redirect successfull

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, redirect,render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -14,8 +14,10 @@ class User(db.Model):
     id =db.Column(db.Integer(), primary_key=True)
     first_name=db.Column(db.String(length=30), nullable=False)
     second_name=db.Column(db.String(length=30), nullable=False)
-
-
+    email=db.Column(db.String(length=30), nullable=False)
+    pswd=db.Column(db.String(length=30), nullable=False)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 @app.route('/welcome_page')
@@ -26,10 +28,10 @@ def welcome_page():
 def sign_up():
     error=None
     if request.method =='POST':
-        Firstname=request.form.get('firstname')
-        Lastname=request.form.get('lastname')
-        Email=request.form.get('email')
-        Passwd=request.form.get('password')
+        Firstname=request.form['firstname']
+        Lastname=request.form['lastname']
+        Email=request.form['email']
+        Passwd=request.form['password']
 
         for i in Firstname:
             numbers=('1','2','3','4','5','6','7','8','9','0')
@@ -38,16 +40,26 @@ def sign_up():
                 error='names should not contain numbers'
             elif i in signs:
                 error='names should not contain signs'
-            elif len('Firstname')<2:
-                error='Your name must atleast contain 2 characters'
-            elif len('Firstname')>20:
-                error='Your name must atmost contain 20 characters'
-            elif '@' not in Email:
-                error='invalid email: Emails must contain "@" character'
-            elif len(Passwd)<8:
-                error= 'Your password must atleast have 8 characters'
             else:
                 pass
+
+        if len('Firstname')<2:
+            error='Your name must atleast contain 2 characters'
+        elif len('Firstname')>20:
+            error='Your name must atmost contain 20 characters'
+        elif len(Passwd)<8:
+            error= 'Your password must atleast have 8 characters'
+        else:
+             pass
+        user=User(first_name=Firstname,
+                  second_name=Lastname,
+                  email=Email,
+                  pswd=Passwd)
+        
+        db.session.add(user)
+        db.session.commit()
+    
+        return redirect(url_for('user_list',id=user.id,first_name=user.first_name, email=user.email ))
 
     return render_template('sign_up.html', error=error)
 
@@ -56,6 +68,11 @@ def sign_up():
 def login():
     
     return render_template('login.html')
+
+@app.route('/user_details')
+def user_list():
+    
+    return render_template('list.html')
 
 
 if __name__=="__main__":

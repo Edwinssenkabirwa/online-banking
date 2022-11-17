8a5b621fb86466913a10542944e6647be8d52d46
Edwinssenkabirwa <edwinssenkabirwa@gmail.com>
2022-11-17 15:19:51 +0300

    still form not validated

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask,render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+
+db =SQLAlchemy()
+
 app=Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///bank.db'
-db =SQLAlchemy(app)
+
+db.init_app(app)
 
 
-class Users(db.Model):
+class User(db.Model):
     id =db.Column(db.Integer(), primary_key=True)
     first_name=db.Column(db.String(length=30), nullable=False)
     second_name=db.Column(db.String(length=30), nullable=False)
